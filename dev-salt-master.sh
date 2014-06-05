0f25381a84a44c04df27e3749dfbbbb5118d05c6
drawsmcgraw <drew.malone@gmail.com>
2014-06-04 22:19:24 -0400

    Configures minion before starting.

--- dev-salt-master.sh
+++ dev-salt-master.sh
@@ -1,13 +1,13 @@
 # Install some pre-reqs
 yum install -y unzip wget git GitPython vim
 git config --global user.name  "drawsmcgraw"
-git config --global user.email "drawsmcgraw"
 
 # Fetch, then run, the Salt-bootstrap script
 # -M to install salt-master
 # -L to also install apache-libcloud (for salt-cloud)
+# -X to *not* start the daemons
 wget https://raw.githubusercontent.com/saltstack/salt-bootstrap/stable/bootstrap-salt.sh
-sh bootstrap-salt.sh -M -L
+sh bootstrap-salt.sh -M -L -X
 
 # Install salt-vim files
 git clone https://github.com/saltstack/salt-vim.git
@@ -15,3 +15,11 @@ if [ ! -d ~/.vim ]; then
 mkdir ~/.vim
 fi
 cp -r salt-vim/* ~/.vim/.
+
+# Minion should connect to localhost
+echo '127.0.0.1 salt' >> /etc/hosts
+
+service salt-master start
+service salt-minion start
+
+echo "Done. Be sure to set git user.email and fix file_roots."
